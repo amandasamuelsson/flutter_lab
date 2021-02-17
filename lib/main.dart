a78c8c2fcd2d0ed153983572c2e407dc3206cf3a
Amanda <amanda.e.samuelsson@hotmail.com>
2021-02-17 17:48:05 +0100

    Added content and drawer menu

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,69 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final appTitle = 'Restaurang Dansken';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primaryColor: Colors.red[800]),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Restaurang Dansken'),
-        ),
-        body: Center(
-          child: Text('Välkommen till en unik restaurang'),
+      title: appTitle,
+      theme: ThemeData(
+        primaryColor: Colors.red[800],
+      ),
+      home: MyHomePage(title: appTitle),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: Column(children: <Widget>[
+        Image.asset('assets/denmark-01.jpeg'),
+        Text('Välkommen till en unik restaurang',
+            style: TextStyle(fontSize: 24.0)),
+      ]),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Image(
+                  image: NetworkImage(
+                      'http://danskengbg.se/content/themes/bliss/img/logo.png')),
+              decoration: BoxDecoration(
+                color: Colors.white,
+              ),
+            ),
+            ListTile(
+              title: Text('Våran meny'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Kontakta oss'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context, '/a');
+              },
+            ),
+          ],
         ),
       ),
     );
