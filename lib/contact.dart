65f694cb43cd27f73a8ce8f0dbe2ceaf05df3e36
Amanda <amanda.e.samuelsson@hotmail.com>
2021-02-18 10:22:32 +0100

    Added content to contact page

--- lib/contact.dart
+++ lib/contact.dart
@@ -7,16 +7,29 @@ class ContactPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Kontakta oss"),
       ),
-      body: Center(
-        child: ElevatedButton(
-          style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all<Color>(Colors.red[800]),
+      body: Column(
+        children: <Widget>[
+          Image.network(
+            'http://danskengbg.se/content/themes/bliss/img/logo.png',
           ),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Tillbaka'),
-        ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+            child: Text(
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+              style: TextStyle(fontSize: 16),
+            ),
+          ),
+          ElevatedButton(
+            style: ButtonStyle(
+              backgroundColor:
+                  MaterialStateProperty.all<Color>(Colors.red[800]),
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text('Tillbaka'),
+          ),
+        ],
       ),
     );
   }
